34a6bb61894fcd3ae4493dfba1c251ba17e628a6
Mutuba <danielmutubait@gmail.com>
2023-05-23 08:58:24 +0300

    fix styles for cards

--- app/services/deposit_mpesa_transaction_service.rb
+++ app/services/deposit_mpesa_transaction_service.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-# class DepositMpesaTransactionService
 class DepositMpesaTransactionService < ApplicationService
-  attr_reader :amount, :sender, :mpesa_account, :success, :error
+  attr_reader :amount, :sender, :success, :error
 
   def initialize(params)
     @amount = params[:amount].to_i
@@ -11,19 +10,29 @@ class DepositMpesaTransactionService < ApplicationService
 
   def call
     ActiveRecord::Base.transaction do
-      # Create MpesaTransaction
-      mpesa_transaction = MpesaTransaction.create!(amount: @amount, sender: @sender, receiver: @sender)
-      # Update mpesa balance
-      mpesa_account = sender&.mpesa_account
-      existing_balance = mpesa_account.available_balance.to_f
-      balance_after_deposit = existing_balance + mpesa_transaction.amount.to_f
-      mpesa_account.update!(available_balance: balance_after_deposit)
-      mpesa_transaction.update!(complete: true)
+      mpesa_transaction = create_transaction
+      update_balance(mpesa_transaction)
+      mark_transaction_completed(mpesa_transaction)
+
       OpenStruct.new(success: true, error: false)
     rescue StandardError => e
-      # Rollback the transaction if an error occurs
       ActiveRecord::Base.connection.rollback_db_transaction
       OpenStruct.new(success: false, error: e.message)
     end
+  end
+
+  private
+
+  def create_transaction
+    MpesaTransaction.create!(amount: @amount, sender: @sender, receiver: @sender)
+  end
+
+  def update_balance(transaction)
+    mpesa_account = sender&.mpesa_account
+    mpesa_account.update!(available_balance: mpesa_account.available_balance.to_f + transaction.amount.to_f)
+  end
+
+  def mark_transaction_completed(transaction)
+    transaction.update!(complete: true)
   end
 end
