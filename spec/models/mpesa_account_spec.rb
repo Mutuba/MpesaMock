b6e25ea544299b320a103f1a8290d28526faff01
Mutuba <danielmutubait@gmail.com>
2023-05-28 18:02:07 +0300

    add mpesa_account model tests

--- spec/models/mpesa_account_spec.rb
+++ spec/models/mpesa_account_spec.rb
@@ -3,5 +3,14 @@
 require 'rails_helper'
 
 RSpec.describe MpesaAccount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to :user }
+    it { should validate_presence_of :user_id }
+    it { is_expected.to respond_to :user }
+  end
+
+  describe 'Validations' do
+    subject { build(:mpesa_account) }
+    it { is_expected.to allow_value(0.0).for(:available_balance) }
+  end
 end
